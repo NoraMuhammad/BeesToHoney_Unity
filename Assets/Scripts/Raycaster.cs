8f1a6c72d4890b58dc4aa037a7ef6c5b79d4ee7a
Nora Muhammad <noramohamedfcis@gmail.com>
2020-04-11 15:48:29 +0200

    Working raycaster is added to the main camera

---
+++ Assets/Scripts/Raycaster.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Raycaster : MonoBehaviour
+{
+    [SerializeField] GameObject RaycastSpot;
+    [HideInInspector] public static Transform HitPoint;
+    RaycastHit hit;
+    GameObject spot;
+    private void Start()
+    {
+        hit = new RaycastHit();
+        HitPoint = hit.transform;
+        spot = Instantiate(RaycastSpot, hit.point, Quaternion.identity);
+        spot.SetActive(false);
+    }
+    void Update()
+    {
+        if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Playground")))
+        {
+            spot.SetActive(true);
+            spot.transform.position = hit.point;
+            HitPoint = hit.transform;
+        }
+        else if(spot != null)
+        {
+            spot.SetActive(false);
+        }
+    }
+}
